835ccb29159ce787496cf273d8ae7f66c12ea606
Samuel Amihere <amisam2000@gmail.com>
2024-01-14 20:44:16 +0800

    API status

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+
+    const amenityIds = {};
+
+    $('input[type=checkbox]').change(function () {
+
+        const checked = $(this).prop('checked');
+        const dataId = $(this).attr('data-id');
+        const dataName = $(this).attr('data-name');
+
+        if (checked) {
+            amenityIds[dataId] = dataName;
+        } else if (!checked) {
+            delete amenityIds[dataId];
+        }
+
+        obj = Object.values(amenityIds);
+        key_len = obj.length;
+        h4 = $('div.amenities h4');
+
+        key_len === 0 ? h4.html('&nbsp;') : h4.text(obj.join(', '));
+
+    });
+
+
+    // Request API status
+    $.get('http://' + window.location.hostname + ':5001/api/v1/status/', function(data) {
+        if (data.status === 'OK') {
+            $('div#api_status').addClass('available');
+        } else {
+            $('div#api_status').removeClass('available');
+        }
+    });
+
+});
